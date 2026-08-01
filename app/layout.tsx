4b046f636418af251d729841971eb896303cae8d
kjurkows <kjurkows@student.42warsaw.pl>
2026-08-01 15:00:32 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import { Roboto_Slab, Roboto, JetBrains_Mono } from "next/font/google"
+
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+
+const robotoSlab = Roboto_Slab({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-heading",
+})
+
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" })
+
+const fontMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        roboto.variable,
+        robotoSlab.variable
+      )}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
